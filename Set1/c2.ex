67643cbe50fbe70d5276e098d4272980c65a7290
ChadoNihi <chadoishere@gmail.com>
2017-01-22 19:34:14 +0200

    save

--- Set1/c2.ex
+++ Set1/c2.ex
@@ -15,6 +15,29 @@ defmodule HexToBase64 do
   end
 
   defp hex_str_to_bin_list(hex_str) do
-    
+    String.graphemes(hex_str)
+    |> List.foldr([], fn(hex_d, acc) -> [hex_dig_to_bin(hex_d) | acc] end)
+    |> Enum.flatten
+  end
+
+  defp hex_dig_to_bin(d) do
+    case String.downcase(d) do
+      "0" -> [0,0,0,0]
+      "1" -> [0,0,0,1]
+      "2" -> [0,0,1,0]
+      "3" -> [0,0,1,1]
+      "4" -> [0,1,0,0]
+      "5" -> [0,1,0,1]
+      "6" -> [0,1,1,0]
+      "7" -> [0,1,1,1]
+      "8" -> [1,0,0,0]
+      "9" -> [1,0,0,1]
+      "a" -> [1,0,1,0]
+      "b" -> [1,0,1,1]
+      "c" -> [1,1,0,0]
+      "d" -> [1,1,0,1]
+      "e" -> [1,1,1,0]
+      "f" -> [1,1,1,1]
+    end
   end
 end
